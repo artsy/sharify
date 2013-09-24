cb66fa4c1eaf608e336b4f00543486fbcb940586
Craig Spaeth <craigspaeth@gmail.com>
2013-09-24 13:45:44 -0400

    I'll get this API down...

--- example/public/bundle.js
+++ example/public/bundle.js
@@ -10,24 +10,19 @@ $(function() {
   });
 });
 },{"../":2}],2:[function(require,module,exports){
-module.exports = function(obj) {
-  for(var key in obj) {
-    module.exports.data[key] = obj[key];
-  }
-  module.exports.script = '<script type="text/javascript">' + 
-                          'window.__sharifyData = ' + JSON.stringify(module.exports.data) + ';' +
-                          '</script>';
-  return module.exports;
-};
-
-module.exports.use = function(app) {
-  app.locals.sharify = module.exports;
-  return module.exports;
+module.exports = function(data) {
+  return function(req, res, next) {
+    if (typeof data == 'function') data = data(req);
+    module.exports.data = res.locals.sd = data;
+    res.locals.sharifyScript = '' +
+      '<script type="text/javascript">' + 
+        'window.__sharifyData = ' + JSON.stringify(data) + ';' +
+      '</script>';
+    next();
+  };
 };
 
 module.exports.data = {};
-
-module.exports.script = '';
 
 // Client-side code to be run when bundled via browserify.
 // Injects the bootstrapped data into the sharify.data hash.
